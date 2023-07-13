a1173eb14c112b49b9dc9ec2caf700b930c28c78
jmg5909 <124036772+jmg5909@users.noreply.github.com>
2023-07-13 18:27:07 +0900

    0713 기기이름 변경기능 추가

--- app/src/main/java/com/example/smart_cocofarm/device/DvSettingActivity.kt
+++ app/src/main/java/com/example/smart_cocofarm/device/DvSettingActivity.kt
@@ -1,11 +1,18 @@
 package com.example.smart_cocofarm.device
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.smart_cocofarm.MainActivity
 import com.example.smart_cocofarm.R
+import com.example.smart_cocofarm.common.CommonVal
+import com.example.smart_cocofarm.conn.CommonConn
 import com.example.smart_cocofarm.databinding.ActivityDvSettingBinding
+import com.example.smart_cocofarm.domain.MyDeviceVO
+
 
 class DvSettingActivity : AppCompatActivity() {
     var binding: ActivityDvSettingBinding? = null
@@ -27,7 +34,29 @@ class DvSettingActivity : AppCompatActivity() {
             clsBuilder.setView(txtEdit)
             clsBuilder.setPositiveButton("확인") { dialog, which ->
                 val strText = txtEdit.text.toString()
-                // DB처리
+
+                val vo = MyDeviceVO()
+                vo.member_no = CommonVal.loginMember!!.member_no
+                vo.mydevice_name = strText
+                vo.mydevice_id = dvid as Int
+
+                val conn = CommonConn(this@DvSettingActivity, "/device/dvname_modify.and")
+                conn.addParam("member_no", CommonVal.loginMember!!.member_no)
+                conn.addParam("mydevice_name", strText)
+                conn.addParam("mydevice_id", dvid)
+
+                conn.onExecute(object : CommonConn.ConnCallback {
+                    override fun onResult(isResult: Boolean, data: String?) {
+                        if (!isResult) {
+                            return
+                        } else {
+                            val intent = Intent(this@DvSettingActivity, MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            startActivity(intent)
+                            Toast.makeText(this@DvSettingActivity, "기기 이름이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
             }
             clsBuilder.setNegativeButton("취소") { dialog, which -> }
             clsBuilder.setCancelable(false)
--- app/src/main/java/com/example/smart_cocofarm/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/smart_cocofarm/profile/ProfileFragment.kt
@@ -2,13 +2,16 @@ package com.example.smart_cocofarm.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.smart_cocofarm.LoginActivity
 import com.example.smart_cocofarm.R
+import com.example.smart_cocofarm.common.CommonVal
 import com.example.smart_cocofarm.common.CommonVal.loginMember
+import com.example.smart_cocofarm.conn.CommonConn
 import com.example.smart_cocofarm.databinding.FragmentProfileBinding
 import com.example.smart_cocofarm.domain.MemberVO
 import com.example.smart_cocofarm.util.ConfirmDialog
@@ -22,7 +25,21 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        binding.username.text = CommonVal.loginMember?.nickname.toString()
 
+        val conn = CommonConn(requireContext(), "/device/cnt_device.and")
+        conn.addParam("member_no", CommonVal.loginMember?.member_no)
+        conn.onExecute(object : CommonConn.ConnCallback {
+            override fun onResult(isResult: Boolean, data: String?) {
+                if (!isResult) {
+                    binding.count.text = "알 수 없음"
+                    return
+                } else {
+                    Log.d("갯수", "onResult: $data")
+                    binding.count.text = data + "개 기기"
+                }
+            }
+        })
 
         // 법률 정보
         binding.selectLaw.setOnClickListener {
